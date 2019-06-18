41acca42e760bd67145b057effd0963a9b7a5056
greendb16 <greendb16@gmail.com>
2019-06-18 16:43:27 -0400

    Final Commit

--- src/LockerProblem.java
+++ src/LockerProblem.java
@@ -1,27 +1,38 @@
 public class LockerProblem {
-    public static void main(String[]args){
-        boolean [] lockers = new boolean[100];
+    public static void main(String[] args) {
+        boolean[] lockers = new boolean[101];
 
-        for (int i = 0; i < lockers.length; i++) {
-            lockers[i] =true;
+        //Set all lockers to open
+        for (int i = 1; i < lockers.length; i++) {
+            lockers[i] = true;
         }
-        for(int n=1; n<lockers.length; n++){
-        for (int i =n; i<lockers.length; i++){
-            if(lockers[i] = true){
-                lockers[i] = false;
-            }
-            else{lockers[i] = true;}
 
-            int p=0;
-            while(p<n){
-                p++;
-                i++;
-        }}}
-    for(int i=0; i<lockers.length; i++){
-            if(lockers[i] = true){
-                System.out.println("Locker Number "+ (i +1) + " is Open");
+        //Start at second locker.  Changing every nth locker.
+
+//        for (int i = 2; i <= 100; i++) {
+//            for (int j = 1; i * j <= 100; j++) {
+//                lockers[i * j] = (lockers[i * j] == true) ? false : true;
+//            }
+//        }
+        for (int n = 2; n < lockers.length; n++) {
+            for (int i = n; i < lockers.length; i+=n) {
+                lockers[i] = !lockers[i];
             }
-            else{
-                System.out.println("Locker Number "+ (i+1) + " is closed");
+
+//                int p = 0;
+//                while (p < n) {
+//                    p++;
+//                    i++;
+//                }
+
+
+        //print locker status
+        for (int i = 1; i < lockers.length; i++) {
+            if (lockers[i]) {
+                System.out.println("Locker Number " + (i) + " is Open");
+            } else {
+                System.out.println("Locker Number " + (i) + " is closed");
+            }
+        }
     }
-    }}}
+}}
